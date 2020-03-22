c41cd84dac0f1179a1e7e2693f27f02578f74f4f
Robert <robert.bonagura@gmail.com>
2020-03-22 17:13:27 -0400

    Add margin to UI and updated title

--- src/program3/Main.java
+++ src/program3/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Program 3 - Tuition Manager");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
 
