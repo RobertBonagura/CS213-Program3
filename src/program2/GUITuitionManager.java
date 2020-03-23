17d16892c968cc59f2f15d1f7f4b4674674936e4
EzraHaleva <HalevaEzra@gmail.com>
2020-03-23 17:57:29 -0400

    added handling for error messages.

--- src/program2/GUITuitionManager.java
+++ src/program2/GUITuitionManager.java
@@ -1,49 +1,64 @@
 package program2;
 
-import program2.Instate;
-import program2.International;
-import program2.Outstate;
-import program2.Student;
-import program2.StudentList;
-
 public class GUITuitionManager {
+    // The use case I envisioned is that the controller will send the appropriate request from
+    // the input provided in the GUI by calling the methods here. For error checking, each of the
+    // request methods returns a string. If the string is empty, there is no error. If it isn't
+    // equivalent to the empty string, there is an error and the returned string is the appropriate
+    // error message.
     StudentList studentList;
+    private final String NO_ERROR = "";
     public GUITuitionManager(){
        this.studentList = new StudentList();
     }
-    public void addInstateRequest(String fName, String lName, int numCredits, int funding){
+    public String addInstateRequest(String fName, String lName, int numCredits, int funding){
         Instate studentToAdd = new Instate(fName, lName, numCredits, funding);
-        if(addRequestIsValid(studentToAdd, studentList)){
+        String errorMsg = determineAddRequestErrorMsg(studentToAdd, studentList);
+        if(errorMsg.equals(NO_ERROR)){
             studentList.add(studentToAdd);
+            return NO_ERROR;
+        } else {
+            return errorMsg;
         }
     }
-    public void addOutstateRequest(String fName, String lName, int numCredits, boolean isTristate){
+    public String addOutstateRequest(String fName, String lName, int numCredits, boolean isTristate){
         Outstate studentToAdd = new Outstate(fName, lName, numCredits, isTristate);
-        if(addRequestIsValid(studentToAdd, studentList)){
+        String errorMsg = determineAddRequestErrorMsg(studentToAdd, studentList);
+        if(errorMsg.equals(NO_ERROR)){
             studentList.add(studentToAdd);
+            return NO_ERROR;
+        } else {
+            return errorMsg;
         }
     }
-    public void addInternationalRequest(String fName, String lName, int numCredits, boolean isExchange){
+    public String addInternationalRequest(String fName, String lName, int numCredits, boolean isExchange){
         International studentToAdd = new International(fName, lName, numCredits, isExchange);
-        if(addRequestIsValid(studentToAdd, studentList)){
+        String errorMsg = determineAddRequestErrorMsg(studentToAdd, studentList);
+        if(errorMsg.equals(NO_ERROR)){
             studentList.add(studentToAdd);
+            return NO_ERROR;
+        } else {
+            return errorMsg;
         }
     }
-    private boolean addRequestIsValid(Student student, StudentList studentList){
+    private String determineAddRequestErrorMsg(Student student, StudentList studentList){
         if(student.credit < 1){
-            return false;
+            return "Error: Student must have a positive number of credits.";
         } else if( (student instanceof International) && student.credit< 9 ) {
-            return false;
+            return "Error: International students must have at least 9 credits.";
         } else if (studentList.contains(student)){
-            return false;
+            return "Error: Student could not be added because list already contains this student.";
         }
-        return true;
+        return NO_ERROR;
     }
-    public void sendDeleteRequest(String fName, String lName){
+    public String sendDeleteRequest(String fName, String lName){
         int NULL = 0;
         Student studentToRemove = new Instate(fName, lName, NULL, NULL);
         if(studentList.contains(studentToRemove)){
             studentList.remove(studentToRemove);
+            return NO_ERROR;
+        } else {
+            return "Error: Could not remove student because student is not present in list.";
         }
     }
     public void sendPrintRequest(){
